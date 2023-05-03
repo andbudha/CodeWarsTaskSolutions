a59dc46e6a18e1aa1e4713bb326341e17d54a4ad
andbudha <andreibartov@gmail.com>
2023-05-03 21:30:20 +0200

    codewars.js updated

--- codewars.js
+++ codewars.js
@@ -76,3 +76,26 @@ const getCount = (str) => {
 }
 
 console.log(getCount(name));
+
+
+/*
+Task 2
+Description:
+Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+Examples
+remove("Hi!") == "Hi"
+remove("Hi!!!") == "Hi!!"
+remove("!Hi") == "!Hi"
+remove("!Hi!") == "!Hi"
+remove("Hi! Hi!") == "Hi! Hi"
+remove("Hi") == "Hi"
+ */
+
+const string = 'Hi John!'
+
+function remove (str) {
+    //coding and coding....
+    return str.at(-1) === '!' ? str.slice(0, -1) : str;
+}
+
+console.log(remove(string));
