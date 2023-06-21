55860896c002392085ebe24aecae007f7ba98a3f
andbudha <andreibartov@gmail.com>
2023-06-21 21:12:49 +0200

    CodeWars Week 15 Task One carried put

--- codewars.js
+++ codewars.js
@@ -185,3 +185,41 @@ console.log(min(arr));
 const max = (list) => list.sort((a, b) => a > b ? -1 : 1)[0];
 
 console.log(max(arr));
+
+
+//Week 15
+
+/*
+Task 1
+Given two numbers and an arithmetic operator (the name of it, as a string), 
+return the result of the two numbers having that operator used on them.
+a and b will both be positive integers, 
+and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+*/
+
+function arithmetic(a, b, operator) {
+    //your code here!
+    if (operator === 'add') {
+        return a + b;
+    } else if (operator === 'subtract') {
+        return a - b;
+    } else if (operator === 'multiply') {
+        return a * b;
+    } else if (operator === 'divide') {
+        return a / b;
+    }
+}
+
+
+/*
+Task 2
+
+ */
+
--- roughpaper.js
+++ roughpaper.js
@@ -1,1 +1,14 @@
 
+const math = (a, b, operator) => {
+    if (operator === 'add') {
+        return a + b;
+    } else if (operator === 'substract') {
+        return a - b;
+    } else if (operator === 'multiply') {
+        return a * b;
+    } else if (operator === 'devide') {
+        return a / b;
+    }
+}
+
+console.log(math(5, 6, 'multiply'));
