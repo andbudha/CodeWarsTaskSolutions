7dc91645c1ac533922427420c99be895e5ed1a56
andbudha <andreibartov@gmail.com>
2023-08-09 17:13:48 +0200

    week 18/task 1 fullfilled

--- codewars.js
+++ codewars.js
@@ -330,3 +330,32 @@ function nameShuffler(str) {
 }
 
 console.log(nameShuffler('Bartov Andrei'));
+
+
+/*
+Week 18
+
+Task 1
+Get the Middle Character
+Description
+You are going to be given a word. 
+Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+*/
+
+const word = 'Delia';
+
+function getMiddle(string) {
+    //Code goes here!
+    const length = string.split('').length;
+    if (length % 2 === 0) {
+        return string.split('').splice(length / 2 - 1, 2).join('');
+    } else {
+        return string.split('').splice((length - 1) / 2, 1).join('');
+    }
+}
+
+console.log(getMiddle(word));
+
+
