92268991e2a111df0a4029762feb70ccf5f680cc
andbudha <andreibartov@gmail.com>
2023-08-11 16:42:42 +0200

    week 19 / task 2 fullfilled

--- roughpaper.js
+++ roughpaper.js
@@ -1,7 +1,7 @@
-
-const fakeBin = (num) => {
-    if (!num) return false;
-    return Number(num) < 5 ? num = new String(0)[0] : num = new String(1)[0];
+function move(position, roll) {
+    // return the new position
+    return (roll * 2) + position;
 }
-const result2 = fakeBin('5');
-console.log(result2);
+
+
+console.log(move(3, 6));
