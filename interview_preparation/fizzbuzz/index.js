13d0ef0568f229deb45f3c724ff5ac4a300c1ed4
andbudha <andreibartov@gmail.com>
2023-09-16 22:15:19 +0200

    fizz-buzz interviewtask solved

---
+++ interview_preparation/fizzbuzz/index.js
@@ -0,0 +1,58 @@
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+function fizzBuzz(n) {
+  if (n > 0 && n % 3 === 0 && n % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (n > 0 && n % 3 === 0) {
+    return 'fizz';
+  } else if (n > 0 && n % 5 === 0) {
+    return 'buzz';
+  } else {
+    return n;
+  }
+}
+
+console.log(fizzBuzz(9));
+
+//My Solution seems not to work as the tests expect it to work, although the result is correct.
+
+// function fizzBuzz(n) {
+// if(n>=0 && n%3===0 && n%5===0)
+// return 'fizzbuzz'
+// if(n>0 && n%3===0)
+// return 'fizz'
+// if(n>0 && n%5===0)
+// return 'buzz'
+// else
+// return n;
+// }
+
+// console.log(fizzBuzz(5));
+
+//The solution to match the tests
+
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (i % 3 === 0) {
+//       console.log('fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
