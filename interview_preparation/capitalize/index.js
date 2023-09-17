db70a72a2601b56d7fc34c741fc7bb558113c2bb
andbudha <andreibartov@gmail.com>
2023-09-17 14:42:47 +0200

    capitalize interviewtask solved

---
+++ interview_preparation/capitalize/index.js
@@ -0,0 +1,63 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  return str
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
+const string = 'hi, my name is andrei.';
+
+console.log(capitalize(string));
+
+//Path to the solution
+
+/*
+const string = 'hi, my name is andrei.';
+
+const newStr = string
+  .split(' ')
+  .map((word) => word[0].toUpperCase() + word.slice(1))
+  .join(' ');
+console.log(newStr);
+
+*/
+
+//Soltion 1
+/* 
+function capitalize(str) {
+  return str
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+}
+const string = 'hi, my name is andrei.';
+
+console.log(capitalize(string));
+*/
+
+//Solution 2
+
+/*
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result = result + str[i].toUpperCase();
+    } else {
+      result = result + str[i];
+    }
+  }
+  return result;
+}
+const string = 'hi, my name is andrei.';
+
+console.log(capitalize(string));
+*/
